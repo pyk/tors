b0026244c793b07b5a4cfd920c8c4748c2bf734a
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2017-02-18 23:08:30 +0700

    Tensor initialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,30 @@
+pub struct Tensor<T> {
+    shape: Vec<usize>,
+    content: Vec<T>,
+    content_size: usize,
+}
+
 #[cfg(test)]
 mod tests {
+    use super::Tensor;
+
     #[test]
-    fn it_works() {
+    fn tensor_initializer() {
+        // Initialize tensor
+        let shape: Vec<usize> = vec![2, 2];
+        let content_size: usize = shape.iter().fold(0, |acc, &x| acc + x);
+        let mut content: Vec<f32> = Vec::with_capacity(content_size);
+        for i in 0..content_size {
+            content.push(0.0);
+        }
+
+        let t: Tensor<f32> = Tensor{
+            shape: shape,
+            content_size: content_size,
+            content: content,
+        };
+
+        assert_eq!(t.shape, vec![2, 2]);
+        assert_eq!(t.content, vec![0.0, 0.0, 0.0, 0.0]);
     }
 }
